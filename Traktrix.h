e43b7561642d6cd0828ce1d3df339484595863eb
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-10-12 11:53:10 +0300

    Update Traktrix.h

--- Traktrix.h
+++ Traktrix.h
@@ -3,25 +3,40 @@
 #include <iostream>
 
 namespace ns {
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::bad_alloc;
+
     struct Coordinate {
         int x;
         int y;
-        Coordinate(double x = 0, double y = 0): x(x), y(y) {};
+        explicit Coordinate(double x = 0, double y = 0): x(x), y(y) {};
     };
 
     class Traktrix {
     private:
         double a;
     public:
-        Traktrix(double a = 1): a(a) {};
+        explicit Traktrix(double a = 1): a(a) {};
 
-        Coordinate intersection() const;
-        double length() const;
-        double radius() const;
-        double firstSquare() const;
-        double secondSquare() const;
-        double volume() const;
+        [[nodiscard]] Coordinate intersection(double angle) const;
+        [[nodiscard]] double length(double angle) const;
+        [[nodiscard]] double radius(double angle) const;
+        [[nodiscard]] double firstSquare(double angle) const;
+        [[nodiscard]] double secondSquare(double angle) const;
+        [[nodiscard]] double volume(double angle) const;
     };
+
+    template <class T>
+    int enter (T &a) {
+        cin >> a;
+        if (!cin.good()) {
+            cout << "Error with cin \n Try again" << endl;
+            return 1;
+        } else
+            return 0;
+    }
 }
 
 #endif //_TRACTRIX_H
